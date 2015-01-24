6f3ff887956be85559ff14d042f1d64b68050162
unknown <cemen7on@gmail.com>
2015-01-24 15:10:14 +0300

    Art medium previews html was fixed

--- protected/views/art/pictures.php
+++ protected/views/art/pictures.php
@@ -8,7 +8,7 @@
     Yii::app()->clientScript->registerScriptFile('/js/pictures.js');
 
     foreach($works as $key=>$work){?>
-        <div class="button thumb picture float-left <?php echo !Yii::app()->request->isAjaxRequest?'animated':null;?>"
+        <div class="button thumb picture  <?php echo !Yii::app()->request->isAjaxRequest?'animated':null;?>"
              id="art<?php echo $work->id;?>" data-width="<?php echo $work->thumbBig->width;?>"
              data-height="<?php echo $work->thumbBig->height;?>">
 
--- protected/views/art/videos.php
+++ protected/views/art/videos.php
@@ -3,7 +3,7 @@
 
     /** @var array $works */
     foreach($works as $key=>$work){?>
-        <div class="button thumb video float-left <?php echo !Yii::app()->request->isAjaxRequest?'animated':null;?>"
+        <div class="button thumb video <?php echo !Yii::app()->request->isAjaxRequest?'animated':null;?>"
              id="art<?php echo $work->id;?>" data-video-id="<?php echo $work->youtube_id;?>">
 
             <div class="image">
