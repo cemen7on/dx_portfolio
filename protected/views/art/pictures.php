d0057fb42e52ace751bab3b708baeb609af4b593
unknown <cemen7on@gmail.com>
2015-01-24 17:42:58 +0300

    Resizeable thumbs were fixed

--- protected/views/art/pictures.php
+++ protected/views/art/pictures.php
@@ -9,7 +9,7 @@
 
     foreach($works as $key=>$work){?>
         <div id="art<?php echo $work->id;?>" class="button thumb picture"
-             style="<?php echo isset($width)?"{$width}px;":null; echo isset($height)?"{$height}px;":null;?>"
+             style="<?php echo isset($width)?"width: {$width}px;":null; echo isset($height)?"height: {$height}px;":null;?>"
              data-width="<?php echo $work->thumbBig->width;?>" data-height="<?php echo $work->thumbBig->height;?>">
 
             <div class="image <?php echo Yii::app()->request->isAjaxRequest?'animated':null; ?>">
--- protected/views/art/videos.php
+++ protected/views/art/videos.php
@@ -4,7 +4,7 @@
     /** @var array $works */
     foreach($works as $key=>$work){?>
         <div id="art<?php echo $work->id;?>" class="button thumb video"
-             style="<?php echo isset($width)?"{$width}px;":null; echo isset($height)?"{$height}px;":null;?>"
+             style="<?php echo isset($width)?"width:{$width}px; ":null; echo isset($height)?"height:{$height}px;":null;?>"
              data-video-id="<?php echo $work->youtube_id;?>">
 
             <div class="image <?php echo Yii::app()->request->isAjaxRequest?'animated':null; ?>">
