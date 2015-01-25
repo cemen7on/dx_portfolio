0bd9f7a1556ab08d9da6ad5547bf86c490dea968
unknown <cemen7on@gmail.com>
2015-01-25 12:46:15 +0300

    IE gradient was fixed

--- protected/widgets/views/aside.php
+++ protected/widgets/views/aside.php
@@ -3,7 +3,7 @@
 ?>
 
 <aside id="asideNavMenu" class="absolute-height vertical-align">
-    <div class="center">
+    <div class="center align-left">
         <div class="caption"><?php echo $this->caption;?></div>
 
         <ul class="align-left">
