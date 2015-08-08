568224621fe6fc23f0f3f1c320dc16eefe1105c0
unknown <cemen7on@gmail.com>
2015-08-08 16:56:05 +0300

    Facace compiling was fixed

--- protected/models/Facade.php
+++ protected/models/Facade.php
@@ -13,26 +13,38 @@ class Facade extends ActiveRecord{
     }
 
     /**
+     * Returns base criteria instance for compile request
+     *
+     * @return \CDbCriteria
+     */
+    private static function _createCompileCriteria(){
+        $criteria=new \CDbCriteria();
+        $criteria->order='facadeIndex ASC';
+        $criteria->condition='facadeIndex IS NOT NULL';
+
+        return $criteria;
+    }
+
+    /**
      * Compiles new facade object
      *
      * @return bool
      */
     public static function compile(){
-        $criteria=new \CDbCriteria();
-        $criteria->order='facadeIndex ASC';
-        $criteria->condition='facadeIndex IS NOT NULL';
-
         $facadeConfig=\Yii::app()->params['facade'];
 
         $videosModels=new Videos();
         $picturesModels=new Pictures();
 
+        $criteria=self::_createCompileCriteria();
         $criteria->limit=$facadeConfig['animations']['count'];
         $videos=ActiveRecord::toArrayAll($videosModels->findRecords($criteria));
 
+        $criteria=self::_createCompileCriteria();
         $criteria->limit=$facadeConfig['pictures2d']['count'];
         $pictures2d=ActiveRecord::toArrayAll($picturesModels->findAllByTypeId(PicturesType::PICTURES_2D, $criteria));
 
+        $criteria=self::_createCompileCriteria();
         $criteria->limit=$facadeConfig['art3d']['count'];
         $art3d=ActiveRecord::toArrayAll($picturesModels->findAllByTypeId(PicturesType::ART_3D, $criteria));
 
