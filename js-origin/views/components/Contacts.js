648b73367278b7eecb5eea5c865e49859c1576a0
unknown <cemen7on@gmail.com>
2015-08-21 16:38:08 +0300

    Blur for contacts dropdown element was added

--- js-origin/views/components/Contacts.js
+++ js-origin/views/components/Contacts.js
@@ -1,5 +1,12 @@
 use('Views.Components').Contacts=Core.View.extend(function(){
     /**
+     * Contacts list slide duration
+     *
+     * @type {number}
+     */
+    var SLIDE_DURATION=100;
+
+    /**
      * Caption HTML element
      *
      * @type {null|HTMLElement}
@@ -7,20 +14,84 @@ use('Views.Components').Contacts=Core.View.extend(function(){
     this.captionEl=null;
 
     /**
-     * Contacts list HTML element
+     * Contacts list HTML element field
+     *
+     * @type {null|HTMLElement}
+     * @private
+     */
+    var _listEl=null;
+
+    /**
+     * Contacts list HTML element property
      *
      * @type {null|HTMLElement}
      */
-    this.listEl=null;
+    Object.defineProperty(this, 'listEl', {
+        get:function(){
+            return _listEl;
+        },
+        set:function(value){
+            _listEl=value;
+            this.$listEl=$(value);
+        }
+    });
+
+    /**
+     * Whether dropdown is closed at the moment
+     *
+     * @type {boolean}
+     * @private
+     */
+    var _isClosed=true;
 
     this.attributes={
         class:'contacts'
     };
 
     this.events={
-        'click .contacts__caption':function(){
-            $(this.listEl).slideToggle(100);
-        }.bind(this)
+        'click .contacts__caption':'slideToggle'
+    };
+
+    /**
+     * Slides up list element
+     *
+     * @returns {*}
+     */
+    this.slideUp=function(){
+        this.$listEl.slideUp(SLIDE_DURATION);
+
+        _isClosed=true;
+
+        return this;
+    };
+
+    /**
+     * Slides down list element
+     *
+     * @returns {*}
+     */
+    this.slideDown=function(){
+        this.$listEl.slideDown(SLIDE_DURATION);
+
+        _isClosed=false;
+
+        return this;
+    };
+
+    /**
+     * Toggles state of list element
+     *
+     * @returns {*}
+     */
+    this.slideToggle=function(){
+        if(_isClosed){
+            this.slideDown();
+        }
+        else{
+            this.slideUp();
+        }
+
+        return this;
     };
 
     this.render=function(){
@@ -39,6 +110,8 @@ use('Views.Components').Contacts=Core.View.extend(function(){
         contactListBOMEl.appendChild(this.listEl);
 
         this.el.appendChild(contactListBOMEl);
+
+        _addBlurEvent();
     };
 
     /**
@@ -86,4 +159,30 @@ use('Views.Components').Contacts=Core.View.extend(function(){
 
         return listEl;
     };
+
+    /**
+     * Registers click event on body, for simulation of blur event os dropdown,
+     * i.e. when any element outside of dropdown is clicked - dropdown is closed
+     *
+     * @private
+     */
+    var _addBlurEvent=function(){
+        var closure=this;
+
+        document.body.addEventListener('click', function(e){
+            var $target=$(e.target);
+
+            // If drop down is closed - don't need to try anything
+            if(_isClosed){
+                return ;
+            }
+
+            // If it's current element or child of current element
+            if($target==closure.$el || $target.parents('.contacts').size()){
+                return ;
+            }
+
+            closure.slideUp();
+        });
+    }.bind(this);
 });
